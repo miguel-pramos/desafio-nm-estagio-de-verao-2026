08227ee2092e76ea897b16e388dff3e0ecfe773e
Miguel Ramos <m281335@dac.unicamp.br>
2025-11-11 21:55:41 -0300

    debug15

--- frontend/src/app/api/set-cookie-client/route.ts
+++ frontend/src/app/api/set-cookie-client/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
   const token = url.searchParams.get("token");
